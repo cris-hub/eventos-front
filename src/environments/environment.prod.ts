afa954e141fb99b589ba7d6afd0fc4fae82b06ef
cris-hub <crisss198@gmail.com>
2018-11-15 15:56:31 -0500

    enviroments 83 port

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  baseBackEnd: 'http://colsdigitaldocker.cloudapp.net/eventosbackend/',
+  baseBackEnd: 'http://colsdigitaldocker.cloudapp.net:83/eventosbackend/',
   baseManagerContent: 'http://colsgestioncontenidorecreacionprueba.azurewebsites.net/eventos/web/index.php?r='
 
 };
