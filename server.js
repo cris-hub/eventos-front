f1e661ebeae592690aede39792fa6087aa1ffb60
cris-hub <crisss198@gmail.com>
2018-11-22 11:23:59 -0500

    server

--- server.js
+++ server.js
@@ -33,5 +33,10 @@ app.get('/*', function (req, res) {
     res.sendFile(path.join(__dirname + '/dist/eventos/index.html'));
 });
 
+app.post('/', function (req, res) {
+
+    res.sendFile(path.join(__dirname + '/dist/eventos/index.html'));
+});
+
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
