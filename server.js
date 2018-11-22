044c27851b5bf0832c0df1e2ed0cb6d4c90e7330
cris-hub <crisss198@gmail.com>
2018-11-22 12:18:41 -0500

    json

--- server.js
+++ server.js
@@ -36,6 +36,7 @@ app.get('/*', function (req, res) {
 app.post('/', function (req, res) {
 
     console.log(req)
+    console.log(res)
 
     res.sendFile(path.join(__dirname + '/dist/eventos/index.html'));
 });
